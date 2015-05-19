cc7f99e51fcd5c3b4914142ede4dafeffa81db6e
JohnH <john.harrison@intel.com>
2015-05-18 17:11:18 -0700

    Just a file I'm pushing for DaveMcLovinIt

---
+++ source/logic/controller.py
@@ -0,0 +1,47 @@
+__author__ = 'johnhar1'
+
+from time import sleep
+from msvcrt import kbhit
+import pywinusb.hid as hid
+import SendKeys
+from source.data_objects.cl import *
+
+class Controller(object):
+    #
+    def __init__(self):
+        self.poll = False
+        self.cl = ControllerLinks()
+        self.device = None
+        self.devices = hid.find_all_hid_devices()
+
+    #Get the list and return it so a device can be selected
+    def get_devices(self):
+        self.devices = hid.find_all_hid_devices()
+        return self.devices
+
+    #event to be called whena device is chosen to be the usb device
+    def set_device(self,device_id = None):
+        if not isinstance(device_id,int):
+            return
+        self.device = self.devices[device_id]
+        try:
+            self.device.open()
+            self.device.set_raw_data_handler(self.hid_handler)
+
+        except hid.core.HIDError():
+            self.device.close()
+
+    def hid_handler(self, data):
+        if self.
+        #print("Raw data: {0}".format(data))
+
+
+    #Save and load config TBA
+    def save_config(self):
+        pass
+
+    def load_config(self,config):
+        pass
+
+
+
